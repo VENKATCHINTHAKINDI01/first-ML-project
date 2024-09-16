99eabc8f29c10edd9762225d99987903eb35806d
VENKATCHINTHAKINDI <venkatchinthakindi4@gmail.com>
2024-09-16 13:34:06 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import Datatransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
@@ -55,7 +58,10 @@ if __name__=="__main__":
     
     data_transformation = Datatransformation()
     
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(test_arr,train_arr))
     
     
             
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -18,3 +19,24 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            
+            model.fit(X_train, y_train)
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[model_name] = test_model_score  # Use model_name as the key
+            
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
